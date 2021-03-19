12f1f434ae5cf2da2b1e07d6b056b5d643bd12c6
Neslin Baydar <37474673+baydarn@users.noreply.github.com>
2021-03-19 11:46:54 +0300

    Update timer.selector.ts

--- src/app/state/timer.selector.ts
+++ src/app/state/timer.selector.ts
@@ -1,10 +1,9 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-import *as timer from './timer.reducer';
 import { TimerState } from './timer.state';
 
 export const TIMER_STATE = 'timer';
 
-const getTimerState = createFeatureSelector<TimerState>(TIMER_STATE);
+export const getTimerState = createFeatureSelector<TimerState>(TIMER_STATE);
 
 export const getImageNumber = createSelector(getTimerState,(state)=> state.imageNumber); //state'teki ımageurl'i dönüyor bize
 
